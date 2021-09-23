397c93f29f8f065c8230d405a1b5b2a50543d213
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-09-23 23:55:28 +0400

    Judge C++

---
+++ 0001-1000/902 Dərs nailiyyətlərinin səviyyəsi | C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a ;
+	cin >> a;
+	if (a==1 or a==2 or a==3){
+		cout << "Initial";
+	}else if(a==4 or a==5 or a==6){
+		cout << "Average";
+	}else if(a==7 or a==8 or a==9){
+		cout << "Sufficient";
+	}else if(a==10 or a==11 or a==12){
+		cout << "High";
+	}
+}
