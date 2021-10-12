c30f8b0692fff7c8c0d47c4c3999f530d743be49
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-10-13 01:56:19 +0400

    Judge c++

---
+++ 8000-9000/8242 Müsbət, mənfi və sıfır | C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>> n;
+	if (n>0){
+	    cout << "Positive";
+	}else if (n<0){
+	    cout << "Negative";
+	}else{
+	    cout<<"Zero";
+	}
+
+
+}
