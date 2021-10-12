83c2b21a49ba824613d7ce302952b86f91795e24
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-10-13 02:02:16 +0400

    Judge c++

---
+++ 6000-7000/6278 Binaların nömrəsi | C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    if ((n%2!=0 && m%2!=0)||(n%2==0 && m%2==0)){
+        cout<<1;
+    }else{
+        cout<<0;
+    }
+}
