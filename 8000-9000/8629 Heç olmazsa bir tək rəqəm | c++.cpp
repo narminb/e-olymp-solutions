d5d48dccf74e956583914a582604bd94f6f35bfa
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-10-27 18:13:07 +0400

    Judge c++

---
+++ 8000-9000/8629 Heç olmazsa bir tək rəqəm | c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,a,b,c,e;
+	cin>>n;
+	a=n/1000;
+	b=n%1000/100;
+	c=n%100/10;
+	e=n%10;
+	if (a%2!=0 || b%2!=0 || c%2!=0 || e%2!=0){
+		cout << "YES";
+	}else {
+		cout<< "NO";
+	}}
